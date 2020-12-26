5d1a7b2052e50991327e3d7246deb64245f2c5ed
IDonotK <LuKick@126.com>
2020-12-26 15:52:49 +0800

    demo数据

---
+++ demo数据/getEvents.js
@@ -0,0 +1,149 @@
+export const getEvents = () => {
+  return {
+    data: {
+      "events": [
+        {
+          "id": "event1111111111111111111111111aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+          "name": "event1",
+          "createTime": "2020-12-16T06:22:48.000+00:00",
+          "updateTime": "1969-12-31T16:00:00.000+00:00",
+          "label": "Event",
+          "timeStamp": 1608099770281,
+          "isDeleted": false,
+          "eventCount": 0,
+          "severity": "warning",
+          "source": "k8s",
+          "eventDetails": "应用告警",
+          "status": "open"
+        },
+        {
+          "id": "event2",
+          "name": "event2",
+          "createTime": "2020-12-16T06:22:48.000+00:00",
+          "updateTime": "1969-12-31T16:00:00.000+00:00",
+          "label": "Event",
+          "timeStamp": 1608099770281,
+          "isDeleted": false,
+          "eventCount": 0,
+          "severity": "warning",
+          "source": "k8s",
+          "eventDetails": "应用告警",
+          "status": "open"
+        },
+        {
+          "id": "event3",
+          "name": "event3",
+          "createTime": "2020-12-16T06:22:48.000+00:00",
+          "updateTime": "1969-12-31T16:00:00.000+00:00",
+          "label": "Event",
+          "timeStamp": 1608099770281,
+          "isDeleted": false,
+          "eventCount": 0,
+          "severity": "warning",
+          "source": "k8s",
+          "eventDetails": "应用告警",
+          "status": "open"
+        },
+        {
+          "id": "event4",
+          "name": "event4",
+          "createTime": "2020-12-16T06:22:48.000+00:00",
+          "updateTime": "1969-12-31T16:00:00.000+00:00",
+          "label": "Event",
+          "timeStamp": 1608099770281,
+          "isDeleted": false,
+          "eventCount": 0,
+          "severity": "warning",
+          "source": "k8s",
+          "eventDetails": "应用告警",
+          "status": "open"
+        },
+        {
+          "id": "event5",
+          "name": "event5",
+          "createTime": "2020-12-16T06:22:48.000+00:00",
+          "updateTime": "1969-12-31T16:00:00.000+00:00",
+          "label": "Event",
+          "timeStamp": 1608099770281,
+          "isDeleted": false,
+          "eventCount": 0,
+          "severity": "warning",
+          "source": "k8s",
+          "eventDetails": "应用告警",
+          "status": "open"
+        },
+        {
+          "id": "event6",
+          "name": "event6",
+          "createTime": "2020-12-16T06:22:48.000+00:00",
+          "updateTime": "1969-12-31T16:00:00.000+00:00",
+          "label": "Event",
+          "timeStamp": 1608099770281,
+          "isDeleted": false,
+          "eventCount": 0,
+          "severity": "warning",
+          "source": "k8s",
+          "eventDetails": "应用告警",
+          "status": "open"
+        },
+        {
+          "id": "event7",
+          "name": "event7",
+          "createTime": "2020-12-16T06:22:48.000+00:00",
+          "updateTime": "1969-12-31T16:00:00.000+00:00",
+          "label": "Event",
+          "timeStamp": 1608099770281,
+          "isDeleted": false,
+          "eventCount": 0,
+          "severity": "warning",
+          "source": "k8s",
+          "eventDetails": "应用告警",
+          "status": "open"
+        },
+        {
+          "id": "event8",
+          "name": "event8",
+          "createTime": "2020-12-16T06:22:48.000+00:00",
+          "updateTime": "1969-12-31T16:00:00.000+00:00",
+          "label": "Event",
+          "timeStamp": 1608099770281,
+          "isDeleted": false,
+          "eventCount": 0,
+          "severity": "warning",
+          "source": "k8s",
+          "eventDetails": "应用告警",
+          "status": "open"
+        },
+        {
+          "id": "event9",
+          "name": "event9",
+          "createTime": "2020-12-16T06:22:48.000+00:00",
+          "updateTime": "1969-12-31T16:00:00.000+00:00",
+          "label": "Event",
+          "timeStamp": 1608099770281,
+          "isDeleted": false,
+          "eventCount": 0,
+          "severity": "warning",
+          "source": "k8s",
+          "eventDetails": "应用告警",
+          "status": "open"
+        },
+        {
+          "id": "event10",
+          "name": "event10",
+          "createTime": "2020-12-16T06:22:48.000+00:00",
+          "updateTime": "1969-12-31T16:00:00.000+00:00",
+          "label": "Event",
+          "timeStamp": 1608099770281,
+          "isDeleted": false,
+          "eventCount": 0,
+          "severity": "warning",
+          "source": "k8s",
+          "eventDetails": "应用告警",
+          "status": "open"
+        }
+      ],
+      "total": 596
+    }
+  }
+};
